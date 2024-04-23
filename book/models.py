494952a1f4f4012c720e76c0443fdff147de8178
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-04-23 16:53:31 +0700

    add bookapp and model book

--- book/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=75, blank=False, default='')
-    description = models.CharField(max_length=200)
-    author = models.CharField(max_length=75, blank=False, default='')
-    category = models.CharField(max_length=75, blank=False)
--- our_book/settings.py
+++ our_book/settings.py
@@ -12,15 +12,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m-91nsg^^*+jxzwc6*+3qf1k@!=dv)kpt$c9xj0-l*=mi3xein'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book.apps.BookConfig',
+    'bookapp.apps.BookappConfig',
     'rest_framework',
     'corsheaders',
 ]
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'our_book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT':os.getenv('DB_PORT')
     }
 }
 
